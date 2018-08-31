5a99e06ca2b710fa4a8db56b836702fed8797469
Zach Greene <zdg1521@jagmail.southalabama.edu>
2018-08-30 21:20:01 -0500

    Updated Comic Book View to be Strongly typed

--- ComicbookGallery/Controllers/ComicBooksController.cs
+++ ComicbookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicbookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,18 +11,23 @@ namespace ComicbookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider Man";
-            ViewBag.Issue = 700;
-            ViewBag.Description = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist(){ Name = "Dan Slott", Role = "Script" },
+                    new Artist(){ Name = "Humberto Ramos" , Role = "Pencils"},
+                    new Artist(){ Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist(){ Name ="Edgar Delgado" , Role = "Colors"},
+                    new Artist(){ Name ="Chris Eliopoulos" , Role = "Letters"}
+                }
             };
-            return View();
+                
+
+            return View(comicBook);
         }
     }
 }
---
+++ ComicbookGallery/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicbookGallery.Models
+{
+    public class Artist
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
